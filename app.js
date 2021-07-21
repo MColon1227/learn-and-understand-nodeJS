82e28a8bb1815d0b96f8f344fa29694991bbeeba
Marisol Colon <mcolon@foundationmedicine.com>
2021-07-21 13:46:09 -0600

    view it until execise 24

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var firstname = 'Jane';
+//22. Javascript Aside: Immediately Invoked Function Expressions (IIFEs)
+// console.log('22. Javascript Aside: Immediately Invoked Function Expressions (IIFEs)');
+// const firstname = 'Jane';
 
-(function (lastname) {
+// (function (lastname) {
 
-	var firstname = 'John';
-	console.log(firstname);
-	console.log(lastname);
+// 	const firstname = 'John';
+// 	console.log(firstname);
+// 	console.log(lastname);
 	
-}('Doe'));
+// }('Doe'));
 
-console.log(firstname);
+// console.log(firstname);
+
+//23. How Do Node Modules Really Work?: module.exports and require
+console.log('23. How Do Node Modules Really Work?: module.exports and require');
+const greet = require('./greet')
+greet()
